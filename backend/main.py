64062aa66a57e50ae3b0b8cb3d496e263f07c33a
BadGateway5 <khabibtime@rambler.ru>
2025-02-17 00:51:42 +0300

    add redis

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pyshorteners.exceptions
 from backend.utils import get_short_link
-
+import redis
 
 app = FastAPI()
+r = redis.Redis('localhost', port=6379)
 
 
 class Link(BaseModel):
@@ -14,7 +15,13 @@ class Link(BaseModel):
 @app.post('/cutlink')
 def cut_link(link: str):
     try:
-        short_link = get_short_link(link)
+        if r.get(link):
+            print("я в редисе")
+            short_link = r.get(link)
+        else:
+            print("я создал")
+            short_link = get_short_link(link)
+            r.set(link, short_link)
         return {
             "status": "success",
             "result": short_link
--- backend/utils.py
+++ backend/utils.py
@@ -1,5 +1,14 @@
 import pyshorteners
+import redis
+from backend.main import redis
 
-def get_short_link(url):
+def get_short_link(link):
     shortener = pyshorteners.Shortener()
-    return shortener.clckru.short(url)
+
+    if redis.get(link):
+        short_link = redis.get(link)
+    else:
+        short_link = shortener.clckru.short(link)
+        redis.set(link, short_link)
+        
+    return short_link
